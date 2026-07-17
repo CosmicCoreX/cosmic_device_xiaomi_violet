b6bc068fdf026f106b3e1566d0d3b6b16563bb7a
Z E U S <iamdrzeus14@gmail.com>
2026-07-17 06:55:54 -0700

    violet: Initialize for Lunaris Aosp Signed-off-by: Z E U S <iamdrzeus14@gmail.com>

--- lineage_violet.mk
+++ lineage_violet.mk
@@ -8,19 +8,21 @@
 # Inherit from violet device
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
-# Inherit some common SuperiorOS stuff.
-$(call inherit-product, vendor/superior/config/common_full_phone.mk)
-SUPERIOR_BUILDTYPE := OFFICIAL
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_OMX_SERVICE := false
-WITH_GAPPS := false
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+LUNARIS_BUILD_TYPE := UNOFFICIAL
+SURFACE_FLINGER_BOOST := true
+TARGET_ENABLE_BLUR := false
+TARGET_USE_MAPS := true
+TARGET_USE_FILES := true
+
+#GMS
+WITH_GMS := true
 
 # Inherit MiuiCamera
 $(call inherit-product-if-exists, vendor/MiuiCamera/config.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := superior_violet
+PRODUCT_NAME := lineage_violet
 PRODUCT_DEVICE := violet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
